4b1729bcf07b34b99a790667b2d4523782606921
fishman123456 <den9152@yandex.ru>
2023-04-20 23:41:59 +0700

    добавил именование клавиш в форме

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 12F, FontStyle.Italic, GraphicsUnit.Point);
+            textBox1.Location = new Point(12, 307);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(1047, 130);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1084, 461);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -9,16 +9,47 @@ namespace _WF_keyboard_19_04_2021
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Button[] but = new Button[48];
+            func();
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void func()
+        {
+            // запись клавиш в массив по юникоду
+            #region
+            Button[] but = new Button[48];
+            string[] butnamestr = new string[1500];
+            char[] butname = new char[1500];
+            for (int i = 1040; i < 1106; i++) // 1072-1103 строчные русские  1040-1071 прописные русские
+            {
+                butname[i] = Convert.ToChar(i);
+                butnamestr[i] = butname[i].ToString();
+            }
+            // если строка не пустая заполняем текстбокс
+            foreach (string st in butnamestr)
+            {
+                if (st != null)
+                {
+                    textBox1.Text += "\t";
+                    textBox1.Text += st;
+                }
+            }
+            #endregion
+            
+            // создание клавиш
+            #region
             int y = 50, x = 50;
             int buttonWidth = 80;
             int buttonHeight = 50;
-
             for (int k = 0; k < 48; k++)
             {
                 but[k] = new Button();
-                but[k].Text = (k + 1).ToString();
+                but[k].Text = butnamestr[1072 + k]; // пока русские прописные
+                but[k].Name = butnamestr[1072 +k]; // именуем клавиши, потом сделаем обработчик по имени 20-04-2023
                 but[k].Location = new Point(x, y);
                 but[k].Size = new Size(buttonWidth, buttonHeight);
                 this.Controls.Add(but[k]);
@@ -30,6 +61,8 @@ namespace _WF_keyboard_19_04_2021
                 else
                     x += buttonWidth + 2;
             }
+            #endregion
         }
     }
+
 }
